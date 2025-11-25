ce152ff193bdbe693a3c4cd3f29a95d13b5202b1
Dave Sewell <snocorp@gmail.com>
2025-11-25 08:45:00 -0500

    Mock functions

--- test/unit/definers/functions.test.ts
+++ test/unit/definers/functions.test.ts
@@ -1,52 +1,51 @@
-import {describe, expect, test} from 'vitest'
-import {defineDocumentFunction, defineFunction, defineMediaLibraryAssetFunction} from '../../../src/index.js'
+import {afterEach, describe, expect, test, vi} from 'vitest'
+import * as fns from '../../../src/definers/functions.js'
+import * as index from '../../../src/index.js'
+
+vi.mock(import('../../../src/index.js'), async (importOriginal) => {
+  const originalModule = await importOriginal()
+  return {
+    ...originalModule,
+    validateBlueprint: vi.fn(() => []),
+  }
+})
 
 describe('defineFunction', () => {
   describe('happy paths', () => {
     test('should assign src based on name if not provided', () => {
-      const fn = defineFunction({name: 'test'})
+      const fn = fns.defineFunction({name: 'test'})
       expect(fn.src).toEqual('functions/test')
     })
 
     test('should ignore invalid properties', () => {
       // @ts-expect-error Intentionally wrong type
-      const fn = defineFunction({name: 'test', invalid: 'invalid'})
+      const fn = fns.defineFunction({name: 'test', invalid: 'invalid'})
       expect(Object.keys(fn)).not.toContain('invalid')
     })
   })
   describe('sad paths', () => {
-    test('should throw an error if name is not provided', () => {
-      // @ts-expect-error name is required
-      expect(() => defineFunction({})).toThrow('`name` is required')
-    })
-    test('should validate the name when higher-level functions are called', () => {
-      // @ts-expect-error name is required
-      expect(() => defineDocumentFunction({})).toThrow('`name` is required')
-      // @ts-expect-error name is required
-      expect(() => defineMediaLibraryAssetFunction({})).toThrow('`name` is required')
+    afterEach(() => {
+      vi.resetAllMocks()
     })
 
-    test('should throw an error if memory is not a number', () => {
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineFunction({name: 'test', memory: '1'})).toThrow('`memory` must be a number')
-    })
+    test('should throw an error if validateFunction returns an error', () => {
+      const spy = vi.spyOn(index, 'validateFunction').mockImplementation(() => [{type: 'test', message: 'this is a test'}])
+      expect(() => fns.defineFunction({name: 'func-name'})).toThrow('this is a test')
 
-    test('should throw an error if timeout is not a number', () => {
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineFunction({name: 'test', timeout: '1'})).toThrow('`timeout` must be a number')
+      expect(spy).toHaveBeenCalledOnce()
     })
   })
 })
 describe('defineDocumentFunction', () => {
   describe('happy paths', () => {
     test('should create a default publish event with provided filter', () => {
-      const fn = defineDocumentFunction({name: 'test', event: {filter: '_type == "post"'}})
+      const fn = fns.defineDocumentFunction({name: 'test', event: {filter: '_type == "post"'}})
       expect(fn.event).toEqual({on: ['publish'], filter: '_type == "post"'})
     })
 
     test('should throw an error if event keys are defined using a mix of under the event object as well as at the top level', () => {
       expect(() =>
-        defineDocumentFunction({
+        fns.defineDocumentFunction({
           name: 'test',
           event: {on: ['publish']},
           filter: '_type == "post"',
@@ -55,17 +54,17 @@ describe('defineDocumentFunction', () => {
     })
 
     test('should create the event with publish if not provided', () => {
-      const fn = defineDocumentFunction({name: 'test', src: 'test.js'})
+      const fn = fns.defineDocumentFunction({name: 'test', src: 'test.js'})
       expect(fn.event).toEqual({on: ['publish']})
     })
 
     test('should allow for creating events triggered on create, update and delete', () => {
-      const fn = defineDocumentFunction({name: 'test', src: 'test.js', event: {on: ['create', 'update', 'delete']}})
+      const fn = fns.defineDocumentFunction({name: 'test', src: 'test.js', event: {on: ['create', 'update', 'delete']}})
       expect(fn.event.on).toEqual(['create', 'update', 'delete'])
     })
 
     test('should allow for creating events with explicit include* toggles', () => {
-      const fn = defineDocumentFunction({
+      const fn = fns.defineDocumentFunction({
         name: 'test',
         src: 'test.js',
         event: {on: ['update'], includeAllVersions: true, includeDrafts: true},
@@ -75,7 +74,7 @@ describe('defineDocumentFunction', () => {
     })
 
     test('should allow for creating events scoped to a specific dataset', () => {
-      const fn = defineDocumentFunction({
+      const fn = fns.defineDocumentFunction({
         name: 'test',
         src: 'test.js',
         event: {on: ['update'], resource: {type: 'dataset', id: 'myProject.myDataset'}},
@@ -85,7 +84,7 @@ describe('defineDocumentFunction', () => {
     })
 
     test('should allow for creating events explicitly scoped to all datasets', () => {
-      const fn = defineDocumentFunction({
+      const fn = fns.defineDocumentFunction({
         name: 'test',
         src: 'test.js',
         event: {on: ['update'], resource: {type: 'dataset', id: 'myProject.*'}},
@@ -94,36 +93,17 @@ describe('defineDocumentFunction', () => {
       expect(fn.event.resource?.id).toEqual('myProject.*')
     })
   })
+
   describe('sad paths', () => {
-    test('should throw an error if event.on is not an array', () => {
-      expect(() =>
-        // @ts-expect-error Intentionally wrong type
-        defineDocumentFunction({name: 'test', event: {on: 'publish'}}),
-      ).toThrow('`event.on` must be an array')
+    afterEach(() => {
+      vi.resetAllMocks()
     })
 
-    test('should throw an error if event.resource.type is empty or not dataset', () => {
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineDocumentFunction({name: 'test', event: {on: ['update'], resource: {type: 'a', id: 'myProject.*'}}})).toThrow(
-        '`event.resource.type` must be "dataset"',
-      )
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineDocumentFunction({name: 'test', event: {on: ['update'], resource: {id: 'myProject.*'}}})).toThrow(
-        '`event.resource.type` must be "dataset"',
-      )
-    })
+    test('should throw an error if validateDocumentFunction returns an error', () => {
+      const spy = vi.spyOn(index, 'validateDocumentFunction').mockImplementation(() => [{type: 'test', message: 'this is a test'}])
+      expect(() => fns.defineDocumentFunction({name: 'test', event: {on: ['publish']}})).toThrow('this is a test')
 
-    test('should throw an error if event.resource.id is invalid', () => {
-      expect(() =>
-        defineDocumentFunction({name: 'test', event: {on: ['update'], resource: {type: 'dataset', id: 'notEnoughPeriods'}}}),
-      ).toThrow('`event.resource.id` must be in the format <projectId>.<datasetName>')
-      expect(() =>
-        defineDocumentFunction({name: 'test', event: {on: ['update'], resource: {type: 'dataset', id: 'too.many.periods'}}}),
-      ).toThrow('`event.resource.id` must be in the format <projectId>.<datasetName>')
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineDocumentFunction({name: 'test', event: {on: ['update'], resource: {type: 'dataset'}}})).toThrow(
-        '`event.resource.id` must be in the format <projectId>.<datasetName>',
-      )
+      expect(spy).toHaveBeenCalledOnce()
     })
   })
 })
@@ -133,7 +113,7 @@ describe('defineMediaLibraryAssetFunction', () => {
   describe('happy paths', () => {
     test('should create a default publish event with provided filter', () => {
       const event = {filter: '_type == "post"', resource}
-      const fn = defineMediaLibraryAssetFunction({
+      const fn = fns.defineMediaLibraryAssetFunction({
         name: 'test',
         event,
       })
@@ -142,23 +122,23 @@ describe('defineMediaLibraryAssetFunction', () => {
     })
 
     test('should create the event with publish if not provided', () => {
-      const fn = defineMediaLibraryAssetFunction({name: 'test', src: 'test.js', event: {resource}})
+      const fn = fns.defineMediaLibraryAssetFunction({name: 'test', src: 'test.js', event: {resource}})
       expect(fn.event.on).toEqual(['publish'])
     })
 
     test('should allow for creating events triggered on create, update and delete', () => {
-      const fn = defineMediaLibraryAssetFunction({name: 'test', src: 'test.js', event: {on: ['create', 'update', 'delete'], resource}})
+      const fn = fns.defineMediaLibraryAssetFunction({name: 'test', src: 'test.js', event: {on: ['create', 'update', 'delete'], resource}})
       expect(fn.event.on).toEqual(['create', 'update', 'delete'])
     })
 
     test('should allow for creating events with explicit include* toggles', () => {
-      let fn = defineMediaLibraryAssetFunction({
+      let fn = fns.defineMediaLibraryAssetFunction({
         name: 'test',
         src: 'test.js',
         event: {on: ['update'], includeDrafts: true, resource},
       })
       expect(fn.event.includeDrafts).toEqual(true)
-      fn = defineMediaLibraryAssetFunction({
+      fn = fns.defineMediaLibraryAssetFunction({
         name: 'test',
         src: 'test.js',
         event: {on: ['update'], includeDrafts: false, resource},
@@ -166,23 +146,15 @@ describe('defineMediaLibraryAssetFunction', () => {
       expect(fn.event.includeDrafts).toEqual(false)
     })
   })
-  describe('sad paths', () => {
-    test('should throw an error if event.on is not an array', () => {
-      expect(() =>
-        // @ts-expect-error Intentionally wrong type
-        defineMediaLibraryAssetFunction({name: 'test', event: {on: 'publish'}}),
-      ).toThrow('`event.on` must be an array')
-    })
 
-    test('should throw an error if event.resource.type is empty or not media-library', () => {
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineMediaLibraryAssetFunction({name: 'test', event: {on: ['update'], resource: {type: 'a', id: 'ml12345'}}})).toThrow(
-        '`event.resource.type` must be "media-library"',
-      )
-      // @ts-expect-error Intentionally wrong type
-      expect(() => defineMediaLibraryAssetFunction({name: 'test', event: {on: ['update'], resource: {id: 'ml12345'}}})).toThrow(
-        '`event.resource.type` must be "media-library"',
-      )
+  describe('sad paths', () => {
+    test('should throw an error if validateMediaLibraryAssetFunction returns an error', () => {
+      const spy = vi.spyOn(index, 'validateMediaLibraryAssetFunction').mockImplementation(() => [{type: 'test', message: 'this is a test'}])
+      expect(() =>
+        fns.defineMediaLibraryAssetFunction({name: 'test', event: {on: ['publish'], resource: {type: 'media-library', id: 'ml1234'}}}),
+      ).toThrow('this is a test')
+
+      expect(spy).toHaveBeenCalledOnce()
     })
   })
 })
